f0feda7ee34fafb8e8a4f5ecf4b48859a83d45dc
urumadlavasu <urumadlavasu04@gmail.com>
2023-11-15 13:27:50 +0530

    fag

--- src/counter.jsx
+++ src/counter.jsx
@@ -3,7 +3,7 @@ function Counter(){
     return(
         <div style={{border:"1px solid",padding:"10px",margin:"10px"}}>
             <h1>Counter</h1>
-            <h2></h2>
+            
         </div>
     )
 }
